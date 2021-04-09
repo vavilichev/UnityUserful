0726a069cc10c7455ac289935bce25b79240576f
andreyvavilichev@gmail.com <andreyvavilichev@gmail.com>
2021-04-09 17:41:46 +0300

    Storage system created. Local storage (file) supported. Mobile friendly

---
+++ Assets/VavilichevGD/Architecture/Storage/Scripts/Behavior/IStorageBehavior.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace VavilichevGD.Architecture.StorageSystem {
+	public interface IStorageBehavior {
+		void Save(object saveData);
+		void SaveAsync(object saveData, Action callback);
+		object Load(object saveDataByDefault);
+		void LoadAsync(Action<object> callback, object saveDataByDefault);
+	}
+}
---
+++ Assets/VavilichevGD/Architecture/Storage/Scripts/GameData.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+namespace VavilichevGD.Architecture.StorageSystem {
+	[Serializable]
+	public sealed class GameData {
+		
+		// ======== EXAMPLE REPLACE YOUR OWN DATA PLEASE
+		
+		public int valueInt;
+		public float valueFloat;
+		public Vector3 valueVector3;
+		public Vector2 valueVector2;
+
+		public GameData() {
+			// It is necessary to create values by default in constructor.
+			this.valueInt = Random.Range(0, 10);
+			this.valueFloat = Random.Range(0f, 10f);
+			this.valueVector3 = Vector3.down;
+			this.valueVector2 = Vector2.up;
+		}
+		
+		// ======= END OF EXAMPLE
+
+	}
+}
